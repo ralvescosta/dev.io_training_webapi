49bf80ebbd46ab8b3a42801e671de9976d7c2e93
Rafael Costa <rafael.rac.mg@gmail.com>
2021-03-30 07:49:22 -0300

    feat: controller de autenticacao

---
+++ src/DevIO.Api/ViewModels/UserViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevIO.Api.ViewModels
+{
+    public class RegisterUserViewModel
+    {
+        [Required(ErrorMessage ="O Campo {0} é obrigatorio!")]
+        [EmailAddress(ErrorMessage = "O campo {0} esta em formato invalido!")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "O Campo {0} é obrigatorio!")]
+        [StringLength(100, ErrorMessage = "O Campo {0} precisa estar entre {2} e {1} caracteres", MinimumLength = 6)]
+        public string Password { get; set; }
+
+        [Compare("Password", ErrorMessage = "As senhas não conferem!")]
+        public string ConfirmPassword { get; set; }
+    }
+
+    public class LoginUserViewModel
+    {
+        [Required(ErrorMessage = "O Campo {0} é obrigatorio!")]
+        [EmailAddress(ErrorMessage = "O campo {0} esta em formato invalido!")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "O Campo {0} é obrigatorio!")]
+        [StringLength(100, ErrorMessage = "O Campo {0} precisa estar entre {2} e {1} caracteres", MinimumLength = 6)]
+        public string Password { get; set; }
+    }
+}
