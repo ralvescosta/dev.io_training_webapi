3a1271358f555fd75a5d924d83c71025e7f8e55b
Rafael Costa <rafael.rac.mg@gmail.com>
2021-03-30 20:49:22 -0300

    feat: implementando JWT

--- src/DevIO.Api/Configurations/IdentityConfig.cs
+++ src/DevIO.Api/Configurations/IdentityConfig.cs
@@ -1,9 +1,12 @@
 ﻿using DevIO.Api.Data;
 using DevIO.Api.Extensions;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 namespace DevIO.Api.Configurations
 {
@@ -17,6 +20,33 @@ namespace DevIO.Api.Configurations
                     .AddEntityFrameworkStores<ApplicationDbContext>()
                     .AddErrorDescriber<IdentityMensagensPortugues>()
                     .AddDefaultTokenProviders();
+
+            //JWT
+
+            var appSettingsSection = configuration.GetSection("AppSettings");
+            services.Configure<AppSettings>(appSettingsSection);
+
+            var appSettings = appSettingsSection.Get<AppSettings>();
+            var key = Encoding.ASCII.GetBytes(appSettings.Secret);
+
+            services.AddAuthentication(auth => 
+            {
+                auth.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                auth.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(jwt => 
+            {
+                jwt.RequireHttpsMetadata = true;
+                jwt.SaveToken = true;
+                jwt.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidAudience = appSettings.ValidoEm,
+                    ValidIssuer = appSettings.Emissor
+                };
+            });
 
             return services;
         }
